1c4f78feb8934f9df39a81273c78fd2f263c9cc3
Marcel Kamp <marcel.kamp@telekom.de>
2020-04-20 18:49:05 +0200

    initial commit mayoChat Refactoring

---
+++ MayoChat/app/src/main/java/com/sea/mayochat/TabsAccessorAdapter.java
@@ -0,0 +1,74 @@
+package com.sea.mayochat;
+
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsAccessorAdapter extends FragmentPagerAdapter
+{
+
+    public TabsAccessorAdapter(FragmentManager fm)
+    {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                ChatFragment chatsFragment = new ChatFragment();
+                return chatsFragment;
+
+            case 1:
+                GroupFragment groupsFragment = new GroupFragment();
+                return groupsFragment;
+
+            case 2:
+                ContactFragment contactsFragment = new ContactFragment();
+                return contactsFragment;
+
+            case 3:
+                RequestFragment requestsFragment = new RequestFragment();
+                return requestsFragment;
+
+            default:
+                return null;
+        }
+    }
+
+
+    @Override
+    public int getCount()
+    {
+        return 4;
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        switch (position)
+        {
+            case 0:
+                return "Chats";
+
+            case 1:
+                return "Groups";
+
+            case 2:
+                return "Contacts";
+
+            case 3:
+                return "Requests";
+
+            default:
+                return null;
+        }
+    }
+}
+
